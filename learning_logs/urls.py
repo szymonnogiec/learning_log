def5e1ba97a7b5af934d8f99e286c89adf9d3c53
Szymon Nogieć <szymon.nogiec@gmail.com>
2019-10-16 20:39:28 +0200

    playing around with django

---
+++ learning_logs/urls.py
@@ -0,0 +1,13 @@
+"""Defines URL patterns for learning_logs"""
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    # Home page
+    url(r'^$', views.index, name='index'),
+    # Show all topics
+    url(r'^topics/$', views.topics, name='topics'),
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+]
